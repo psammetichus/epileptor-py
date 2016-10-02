8c1b811b102397d9c0c13cf22840e997334f1965
psammetichus <tyson.burghardt@gmail.com>
2016-10-02 12:47:00 -0400

    first commit of epileptor code

---
+++ epileptor.py
@@ -0,0 +1,23 @@
+
+def epifun(y,t):
+  """
+  Function defining the system of ODEs in the epileptor model. The variables in
+  the published model x1, y1, x2, y2, z are respectively renamed y[0] through
+  y[4]. The helper functions f1, f2, and g are defined in nested scope.
+  """
+
+  def f1(a,b):
+    pass
+
+  def f2(a,b):
+    pass
+
+  def g(a):
+    pass
+
+
+  return [
+
+
+      ]
+
